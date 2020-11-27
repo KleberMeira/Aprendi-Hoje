94443ec90cc5dc4b4463449019657fb86f4c6943
Kleber Meira <klebermeira4@gmail.com>
2020-11-26 23:07:58 -0400

    Estudos em Vue

---
+++ Vue/exercicios/Cliente.vue
@@ -0,0 +1,32 @@
+<template>
+
+    <div id="cliente"> 
+        <h2 id="clienteName">Cliente aqui</h2>    
+        <h3>Descricao Cliente</h3>
+    </div>
+
+    </template>
+
+<script>
+
+import OutroComponente from './OutroComponente'
+
+    export default {
+        name: 'Cliente',
+        components:{
+            OutroComponente
+        }
+    
+}
+</script>
+
+<style scoped>
+        #cliente{
+            color: blue;
+            background-color: azure;
+        }
+        #clienteName{
+            width: 500px;
+        }
+
+</style>
---
+++ Vue/exercicios/componentes.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="app">
+        <h1>Criando Coponente</h1>
+        <Cliente/>
+        <Cliente/>
+        <Cliente/>
+        <Cliente/>
+        <Cliente/>
+    </div>
+</template>
+
+
+<script>
+
+import Cliente from './Cliente'
+
+export default {
+    name: 'App',
+    components: {
+        Cliente
+    }
+}
+
+</script>
+
+
+
+<style scoped>
+
+</style>
