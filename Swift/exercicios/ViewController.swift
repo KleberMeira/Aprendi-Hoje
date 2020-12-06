7fe2040086469a79e402830212a00c592dc0e6df
Meira <klebermeira4@gmail.com>
2020-12-06 11:34:13 -0500

    TextField

--- Swift/exercicios/ViewController.swift
+++ Swift/exercicios/ViewController.swift
@@ -2,8 +2,17 @@ import UIKit
 
 class ViewController: UIViewController{
 
-    @IBAction func adcionar(){
-        print("Alimento: --")
+    @IBOutlet var nomeTextField: UITextField! 
+    @IBOutlet var felicidadeTextField: UITextField!
+
+    //objective c
+    //crach = quando o app fecha inesperadamente
+
+    @IBAction func adcionar(_ sender: Any){
+    let nome       = nomeTextField.text 
+    let felicidade = felicidadeTextField.text
+    
+        print("comi \(nome) e fiquei com a felicidade: \(felicidade)")
     }
 
 }
