e7bd35ebf3991f63d4feb109b6a3f7446f4a639d
Kleber Meira <klebermeira4@gmail.com>
2020-12-16 19:18:17 -0400

    inserindo no banco com node

--- Node/controllers/atendimentos.js
+++ Node/controllers/atendimentos.js
@@ -1,10 +1,14 @@
 
+const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Voce está na rota de atendimento'))
 
     app.post('/atendimentos', (req, res) => { 
-        console.log(req.body)
+        //console.log(req.body)
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
         res.send('Voce esta na rota de atendimento fazendo um post')
-        }) 
+        })
 }
