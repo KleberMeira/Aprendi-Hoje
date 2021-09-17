34940969f385e6d3f095196e3b1a7e0943af65a4
Kleber <klebermeira4@gmail.com>
2021-09-16 21:02:39 -0400

    topicoDto

--- Spring/1/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ Spring/1/forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.controller;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,11 +8,15 @@ import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/topicos")
@@ -39,6 +44,15 @@ public class TopicosController {
 	    Topico topico =	form.converter(cursoRepository);
 		topicoRepository.save(topico);
 	}
+
+
+	@GetMapping("/{id}")
+	public TopicoDto detalhar( @PathVariable("id") Long id){
+		Topico topico = topicoRepository.getOne(id);
+		return new TopicoDto(topico);
+	}
+
+
 
 
 }
